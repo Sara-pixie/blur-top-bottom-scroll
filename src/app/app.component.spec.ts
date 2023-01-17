1be035f939c473125433ec23a31cf20016466495
Sara Paranuk <78561671+Sara-pixie@users.noreply.github.com>
2023-01-17 10:37:39 +0100

    working example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'blur-top-bottom-scroll'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blur-top-bottom-scroll');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blur-top-bottom-scroll';
+export class AppComponent implements AfterViewInit {
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    var scrollContainer = document.getElementsByClassName('scroll-container')[0];
+    scrollContainer.addEventListener('scroll', e => {
+      if(e && e.target) {
+        var scrollTop = (e.target as HTMLDivElement).scrollTop;
+        var elementScrollMax: number = (e.target as HTMLDivElement).scrollHeight - (e.target as HTMLDivElement).clientHeight;
+        var scrollBottom = elementScrollMax - scrollTop;
+        (scrollContainer as HTMLDivElement).setAttribute('topScroll', scrollTop.toString());
+        (scrollContainer as HTMLDivElement).setAttribute('bottomScroll', scrollBottom.toString());
+      }
+    });
+
+    //set initial attributes
+    var scrollTop = 0;
+    var elementScrollMax: number = (scrollContainer as HTMLDivElement).scrollHeight - (scrollContainer as HTMLDivElement).clientHeight;
+    var scrollBottom = elementScrollMax - scrollTop;
+    (scrollContainer as HTMLDivElement).setAttribute('topScroll', scrollTop.toString());
+    (scrollContainer as HTMLDivElement).setAttribute('bottomScroll', scrollBottom.toString());
+    // or -> this.scrollTo(100); to trigger the first scroll event listener
+
+  }
+
+  scrollToTop() {
+    this.scrollTo(0);
+  }
+
+  scrollToBottom() {
+    var scrollContainer = document.getElementsByClassName('scroll-container')[0];
+    var elementScrollMax: number = (scrollContainer as HTMLDivElement).scrollHeight - (scrollContainer as HTMLDivElement).clientHeight;
+    this.scrollTo(elementScrollMax);
+  }
+
+  scrollTo(scroll: number) {
+    var scrollContainer = document.getElementsByClassName('scroll-container')[0];
+    scrollContainer.scrollTop = scroll;
+  }
+
 }
